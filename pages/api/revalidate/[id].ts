349554019a5fa315e577876a079828bca95a6011
Luong Duy <duyltsa160109@fpt.edu.vn>
2023-07-09 20:31:34 +0700

    add revalidate

--- pages/api/revalidate/[id].ts
+++ pages/api/revalidate/[id].ts
@@ -5,11 +5,18 @@ type Data = {
   revalidated : boolean
 }
 
+type Msg = {
+  message : string
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | Msg>
 ) {
   console.log('Revalidating detail page...');
+  if(req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({message : 'Invalid secret'})
+  }
   let revalidated = false
   const {query : { id }} = req
   try {
